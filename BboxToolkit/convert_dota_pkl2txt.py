61c7fa7a85fb44cd97b811180a0d3adfffaf41ed
王麒雄（实习） <wangqixiong@xiaohongshu.com>
2022-04-04 12:42:32 +0800

    debug

--- BboxToolkit/convert_dota_pkl2txt.py
+++ BboxToolkit/convert_dota_pkl2txt.py
@@ -8,18 +8,21 @@ if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 for data in datas['content']:
     file_name_txt=data['filename'].replace('.png','.txt')
-    with open(os.path.join(save_dir,file_name_txt),'w') as f:
-        for i in range(len(data['ann']['labels'])):
-            x1=str(int(data['ann']['bboxes'][i][0]))+' '
-            y1=str(int(data['ann']['bboxes'][i][1]))+' '
-            x2=str(int(data['ann']['bboxes'][i][2]))+' '
-            y2=str(int(data['ann']['bboxes'][i][3]))+' '
-            x3=str(int(data['ann']['bboxes'][i][4]))+' '
-            y3=str(int(data['ann']['bboxes'][i][5]))+' '
-            x4=str(int(data['ann']['bboxes'][i][6]))+' '
-            y4=str(int(data['ann']['bboxes'][i][7]))+' '
-            label=CLASSES[data['ann']['labels'][i]]+' '
-            diff=str(int(data['ann']['diffs'][i]))
+    if file_name_txt=='P1214_0000.txt':
+        import pdb
+        pdb.set_trace()
+        with open(os.path.join(save_dir,file_name_txt),'w') as f:
+            for i in range(len(data['ann']['labels'])):
+                x1=str(int(data['ann']['bboxes'][i][0]))+' '
+                y1=str(int(data['ann']['bboxes'][i][1]))+' '
+                x2=str(int(data['ann']['bboxes'][i][2]))+' '
+                y2=str(int(data['ann']['bboxes'][i][3]))+' '
+                x3=str(int(data['ann']['bboxes'][i][4]))+' '
+                y3=str(int(data['ann']['bboxes'][i][5]))+' '
+                x4=str(int(data['ann']['bboxes'][i][6]))+' '
+                y4=str(int(data['ann']['bboxes'][i][7]))+' '
+                label=CLASSES[data['ann']['labels'][i]]+' '
+                diff=str(int(data['ann']['diffs'][i]))
 
-            write_str=x1+y1+x2+y2+x3+y3+x4+y4+label+diff
-            f.write(write_str+'\n')
+                write_str=x1+y1+x2+y2+x3+y3+x4+y4+label+diff
+                f.write(write_str+'\n')
