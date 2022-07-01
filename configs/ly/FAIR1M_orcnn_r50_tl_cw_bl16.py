d55cbde9fa87d8cbf1378e1716926eaf46c62608
ly-980311 <liyan1611480@163.com>
2022-07-01 16:31:32 +0800

    message

--- configs/ly/FAIR1M_orcnn_r50_tl_cw_bl16.py
+++ configs/ly/FAIR1M_orcnn_r50_tl_cw_bl16.py
@@ -4,5 +4,6 @@ _base_ = ['./FAIR1M_orcnn_r50_tl_cw_bl8.py']
 model = dict(roi_head=dict(bbox_head=dict(num_of_instance=16)))
 
 # runner = dict(max_epochs=37)
-# resume_from = 'work_dirs/max_less_05/latest.pth'
+checkpoint_config = dict(interval=1)
+resume_from = 'work_dirs/b16_max_less_10_01/epoch_24.pth'
 work_dir = 'work_dirs/b16_max_less_10_01'
